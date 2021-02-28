c175e5c643bee4f6ebeb6a683b599de8a9e51631
Marcus Souza <marcus.souza@picpay.com>
2021-02-28 12:34:36 -0300

    Create unit test for koin module

---
+++ app/src/test/java/com/souza/marcus/pokedex/di/RemoteDataModuleTest.kt
@@ -0,0 +1,23 @@
+package com.souza.marcus.pokedex.di
+
+import com.souza.marcus.pokedex.remotedata.di.RemoteDataModule
+import org.junit.Before
+import org.junit.Test
+import org.koin.core.context.startKoin
+import org.koin.dsl.koinApplication
+import org.koin.test.check.checkModules
+
+internal class RemoteDataModuleTest {
+
+    @Before
+    fun setup() {
+        startKoin {  }
+    }
+
+    @Test
+    fun `load should provide dependencies on Koin of RemoteDataModule with success`() {
+        koinApplication {
+            RemoteDataModule.load()
+        }.checkModules()
+    }
+}
