82b27aca134067e98723456ecaf7d5a646e95f8e
Marcus Souza <marcus.souza@picpay.com>
2021-03-07 19:02:32 -0300

    Make Test work

--- app/src/androidTest/java/com/souza/marcus/pokedex/feature/home/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/souza/marcus/pokedex/feature/home/HomeFragmentTest.kt
@@ -3,32 +3,42 @@ package com.souza.marcus.pokedex.feature.home
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.souza.marcus.pokedex.R
+import com.souza.marcus.pokedex.feature.home.helper.successJson
 import com.souza.marcus.pokedex.feature.home.robot.HomeRobot
-import com.souza.marcus.pokedex.feature.home.robot.MockRobot
 import com.souza.marcus.pokedex.features.home.presentation.fragment.HomeFragment
+import com.souza.marcus.pokedex.util.BaseTest
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-internal class HomeFragmentTest {
+internal class HomeFragmentTest : BaseTest() {
 
     private fun robots(func: HomeRobot.() -> Unit) = HomeRobot().apply(func)
-    private fun mocks(func: MockRobot.() -> Unit) = MockRobot().apply(func)
 
     @Before
     fun setup() {
-        launchFragmentInContainer<HomeFragment>(themeResId = R.style.Pokedex)
+        super.server()
+        launchFragmentInContainer<HomeFragment>(themeResId = R.style.Theme_AppCompat)
     }
 
     @Test
     fun whenResultIsOk_shouldDisplayRecyclerViewVisible() {
-        mocks {
-            successResponseWith3Itens()
-        }
+        successResponseWith3Itens()
 
         robots {
             checkIfRecyclerViewIsVisible()
+        }
+    }
+
+    private fun successResponseWith3Itens() {
+        webServer.dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return MockResponse().setResponseCode(200).setBody(successJson)
+            }
         }
     }
 }
--- app/src/androidTest/java/com/souza/marcus/pokedex/util/BaseTest.kt
+++ app/src/androidTest/java/com/souza/marcus/pokedex/util/BaseTest.kt
@@ -1,8 +1,6 @@
 package com.souza.marcus.pokedex.util
 
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.After
-import org.junit.Before
 import org.koin.test.KoinTest
 
 private const val MOCK_WEB_SERVER_PORT = 4007
@@ -11,13 +9,11 @@ open class BaseTest : KoinTest {
 
     protected lateinit var webServer: MockWebServer
 
-    @Before
     fun server() {
         webServer = MockWebServer()
         webServer.start(MOCK_WEB_SERVER_PORT)
     }
 
-    @After
     fun tearDown() {
         webServer.shutdown()
     }
